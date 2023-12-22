4ef6dbde0a4eb463626c2c813be5ec00357f5f12
learnspring2023 <137409193+learnspring2023@users.noreply.github.com>
2023-12-22 15:49:14 +0530

    added

---
+++ demo.js
@@ -0,0 +1,16 @@
+console.log("From separate file");
+//Variables
+
+let a = 10;  //Numbers
+let b = 20;
+c = a+ b ;
+console.log(c);
+let name = "Subhash"; //String
+
+let list = [1,2,3,4,5,6] //Array
+let listString = ["Name1", "Name2"]  //Array of strings
+console.log(list);
+console.log(listString[0]);
+
+document.getElementById("demo")
+.innerHTML = c;
